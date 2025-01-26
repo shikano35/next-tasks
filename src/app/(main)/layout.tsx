60df9901d5da1d0a639a1d2b2b727ff7e1df16c1
shikano <pegtar377512@gmail.com>
2025-01-26 22:45:09 +0900

    feat: サイドメニューコンポーネントを追加し、レイアウトを更新

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import SideMenu from '@/components/SideMenu/SideMenu';
 import React from 'react'
 
 const MainLayout = ({
@@ -7,9 +8,7 @@ const MainLayout = ({
 }>) => {
   return (
     <div className='flex h-screen'>
-     <div className='bg-indigo-300'>
-      サイドメニュー
-     </div>
+      <SideMenu />
      <main className='bg-red-300 flex-1 overflow-auto'>
       {children}
       </main>
