c76054f3ea3649525837551cce02877ccbce3f01
shikano <pegtar377512@gmail.com>
2025-01-27 11:16:17 +0900

    feat: タスク編集ページにタスク取得機能を追加

--- src/app/(main)/edit/[id]/page.tsx
+++ src/app/(main)/edit/[id]/page.tsx
@@ -1,12 +1,24 @@
 import EditTaskForm from '@/components/EditTaskForm/EditTaskForm';
+import { TaskDocument } from '@/models/task';
 import React from 'react'
 
 interface Params {
   params: { id: string };
 }
 
-const EditTaskPage = ({ params }: Params) => {
-  // const id = params.id;
+const getTask = async (id: string): Promise<TaskDocument> => {
+  const response = await fetch(`${process.env.API_URL}/tasks/${id}`, {
+    cache: 'no-store',
+  });
+  const data = await response.json();
+  return data.task as TaskDocument;
+}
+
+const EditTaskPage = async ({ params }: Params) => {
+  const id = params.id;
+  const task = await getTask(id);
+  console.log(task);
+
   return (
     <div className='flex flex-col justify-center py-20'>
       <h2 className='text-center text-2xl font-bold'>
