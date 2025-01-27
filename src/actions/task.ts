fac564e852d9e332fea5a5e808db6a9d72a920d3
shikano <pegtar377512@gmail.com>
2025-01-27 11:44:35 +0900

    feat: タスク削除機能を追加

--- src/actions/task.ts
+++ src/actions/task.ts
@@ -47,3 +47,16 @@ export const updateTask = async (id: string, state: FormState, formData: FormDat
 
     redirect("/");
 }
+
+export const deleteTask = async (id: string, state: FormState) => {
+    try {
+        await connectDb();
+        await TaskModel.deleteOne({_id: id});
+    } catch (error) {
+        state.error = "タスクの削除に失敗しました";
+        console.log(error);
+        return state;
+    }
+
+    redirect("/");
+}
