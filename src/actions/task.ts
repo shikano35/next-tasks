e7f548ae10c6b5f622eec077c192a0cb77e809e5
shikano <pegtar377512@gmail.com>
2025-01-27 10:36:11 +0900

    feat: タスク作成機能を追加し、データベースに新しいタスクを保存

---
+++ src/actions/task.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { Task, TaskModel } from "@/models/task";
+import { connectDb } from "@/utils/database";
+import { redirect } from "next/navigation";
+
+export interface FormState {
+    error: string;
+}
+
+export const creatTask = async (state: FormState, formData: FormData) => {
+    const newTask: Task = {
+        title: formData.get("title") as string,
+        description: formData.get("description") as string,
+        dueDate: formData.get("dueDate") as string,
+        isComplete: false,
+    }
+
+    try {
+        await connectDb();
+        await TaskModel.create(newTask);
+    } catch (error) {
+        state.error = "タスクの作成に失敗しました";
+        console.log(error);
+        return state;
+    }
+
+    redirect("/");
+}
